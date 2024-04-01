ea5ab397dea9eb0d9e21a9646fdf4e7eb6e29cb6
Boris Besemer <borisbesemer@gmail.com>
2024-04-01 17:14:51 +0200

    feat(extractor): add support for finding labels files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::fs;
+use std::{env, fs, path::Path};
 use serde_json::{from_str, json, to_string_pretty, Map, Value};
+use regex::Regex;
 use clap::Parser;
+
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -9,12 +11,6 @@ struct Args {
     #[arg(short, long, default_value = "false")]
     watch: bool
 }
-
-// #[derive(Serialize, Deserialize)]
-// enum LanguageValue {
-//     String(String),
-//     Object(Map<String, Value>)
-// }
 
 fn main() {
     let args = Args::parse();
@@ -33,8 +29,49 @@ fn main() {
     let merged_json = json!(merged_data);
     let merged_json_str = to_string_pretty(&merged_json).expect("Unable to serialize JSON");
 
+    find_files();
+
     fs::write("output.json", merged_json_str).expect("Unable to write file");
 
 
 }
 
+fn find_files() {
+    let current_dir = env::current_dir().unwrap();
+
+    let re = Regex::new(r#"([^\.]+)\.labels\.json$"#).unwrap();
+
+    // Recursively search for files matching the regex
+    let mut files: Vec<String> = Vec::new();
+    search_files(&current_dir, &re, &mut files);
+
+    // Process the found files
+    for file in files {
+        // Process each file here
+        println!("Found file: {}", file);
+    }
+}
+
+/// Recursively search for files in a directory that match a regex
+fn search_files(dir: &Path, re: &Regex, files: &mut Vec<String>) {
+    if let Ok(entries) = fs::read_dir(dir) {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                let path = entry.path();
+                if path.is_dir() {
+                    // Recursively search subdirectories
+                    search_files(&path, re, files);
+                } else if let Some(file_name) = path.file_name() {
+                    if let Some(file_name_str) = file_name.to_str() {
+                        if re.is_match(file_name_str) {
+                            // Add the file path to the list
+                            if let Some(file_path) = path.to_str() {
+                                files.push(file_path.to_string());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
